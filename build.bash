1de65f06732465f269aeba88f84ff2e9b18e601b
Skurdydyk <skur@softserveinc.com>
2022-12-21 23:20:04 +0200

    Updated bash scripts

--- build.bash
+++ build.bash
@@ -1,8 +1,1 @@
 docker build -t ros_noetic .
-
-# --env="LIBGL_ALWAYS_INDIRECT=1" 
-
-# source devel/setup.bash &&
-# export ROS_MASTER_URI=http://192.168.0.132:11311 &&
-# export ROS_IP=$(hostname -I | awk '{print $1;}') &&
-# export ROS_HOSTNAME=$ROS_IP 
