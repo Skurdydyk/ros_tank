85e8b511bc29ee14cb9c57cff1519e49071fb874
Skurdydyk <skur@softserveinc.com>
2023-04-20 20:03:15 +0300

    Removed extra folder

--- run_jetson.bash
+++ run_jetson.bash
@@ -11,7 +11,7 @@ docker run -it --rm \
     --volume="/tmp/.docker.xauth:/tmp/.docker.xauth" \
     --volume="$WS_DIR_PATH/ros_tank_control:/ros2_ws/src/ros_tank_control" \
     --volume="$WS_DIR_PATH/ros_tank_description:/ros2_ws/src/ros_tank_description" \
-    --volume="$WS_DIR_PATH/diffdrive_arduino_humble:/ros2_ws/src/diffdrive_arduino" \
+    --volume="$WS_DIR_PATH/diffdrive_arduino:/ros2_ws/src/diffdrive_arduino" \
     --volume="$WS_DIR_PATH/serial:/ros2_ws/src/serial" \
     --env="QT_X11_NO_MITSHM=0" \
     --env="XAUTHORITY=/tmp/.docker.xauth" \
