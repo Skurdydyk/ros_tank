3f6792094fe379bd536238d613f5914ab522de70
Skurdydyk <skur@softserveinc.com>
2023-04-21 20:29:15 +0300

    Change publish_rate

--- ros2_ws/ros_tank_control/launch/ros_tank_control_diff.launch.py
+++ ros2_ws/ros_tank_control/launch/ros_tank_control_diff.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
 
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
-    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','diff_drive_controllers.yaml')
+    controller_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'diff_drive_controllers.yaml')
 
     controller_manager = Node(
         package="controller_manager",
