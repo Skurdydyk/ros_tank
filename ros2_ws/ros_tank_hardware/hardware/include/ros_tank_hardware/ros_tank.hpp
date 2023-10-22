38ef18eb01d820ef72f78148b8cea70f4302c78b
sviat <sviatuy93@gmail.com>
2023-10-22 18:55:37 +0300

    Removed extra code

--- ros2_ws/ros_tank_hardware/hardware/include/ros_tank_hardware/ros_tank.hpp
+++ ros2_ws/ros_tank_hardware/hardware/include/ros_tank_hardware/ros_tank.hpp
@@ -1,17 +1,3 @@
-// Copyright 2020 ros2_control Development Team
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #ifndef ROS_TANK_HARDWARE__ROS_TANK_HPP_
 #define ROS_TANK_HARDWARE__ROS_TANK_HPP_
 
@@ -26,7 +12,6 @@
 #include "rclcpp/macros.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
-#include "ros_tank_hardware/visibility_control.h"
 
 namespace ros_tank_hardware
 {
